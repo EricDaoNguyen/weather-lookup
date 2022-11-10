56cfebf91ac749345025ba7bd25a2bbf2242ebc7
Eric Dao Nguyen <ericdaonguyen@macbook-pro.myfiosgateway.com>
2022-11-10 13:37:07 -0500

    Doc: added Weather class

---
+++ server/api/weather.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+require('dotenv').config({path: './../../../.env'})
+
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+class Weather {
+    /**
+     * Gets the weather data based on the zipcode and which temp system to converge to (imperial/metric system)
+     *
+     * @param {number} zipCode The zipcode used to get the weather info from the api
+     * @param {string} tempMetric This is either "imperial" (use Fahrenheit) or "metric" (use Celsius)
+     * @return {JSON} The data response from the api call
+     */
+
+    getWeatherData = async(zipCode, tempMetric) => {
+        /**
+         * Use get api for "By ZIP code" (https://openweathermap.org/current#zip)
+         * - "us" query = "United States"
+         * - "process.env.WEATHER_KEY" is the api key the .env file
+         * - "units" query can be either imperial (Fahrenheit) or metric (Celsius)
+         */
+
+        let url = `${BASE_URL}?zip=${zipCode},us&appid=${process.env.WEATHER_KEY}&units=${tempMetric}`
+
+        return(await axios.get(url)).data
+    }
+}
+
+module.exports = Weather
--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
-function server() {
-    require('dotenv').config()
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import apiRoutes from './api'
 
-    const express = require('express')
-    const bodyParser = require('body-parser')
+require('dotenv').config()
 
-    const app = express()
-    const port = process.env.PORT || 3000
+const app = express()
+const port = process.env.PORT || 3000
 
-    // get data from POST requests with bodyParser
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
+// get data from POST requests with bodyParser
+app.use(json())
+app.use(urlencoded({ extended: true }))
 
-    // use api routes from api folders
-    const apiRoutes = require('./api')
-    app.use('/api', apiRoutes)
+// use api routes from api folders
+app.use('/api', apiRoutes)
 
-    app.listen(port, () => console.log(`Listening on port ${port}`))
-}
+app.listen(port, () => console.log(`Listening on port ${port}`))
