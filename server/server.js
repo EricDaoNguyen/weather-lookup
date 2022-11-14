e4e27dc483c387f0ff26d6eed5232ca02d03d88e
Eric Dao Nguyen <ericdaonguyen@Erics-M1-MacBook-Pro.local>
2022-11-14 16:16:16 -0500

    Doc: connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,5 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // use api routes from api folders
 const apiRoutes = require('./api')
 app.use('/api', apiRoutes)
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected!')).catch(error => console.log(error))
 
 app.listen(port, () => console.log(`Listening on port ${port}, up and running!`))
