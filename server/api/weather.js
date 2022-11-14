401d679bdae69154e8cfbfa85443567551558caf
Eric Dao Nguyen <ericdaonguyen@Erics-M1-MacBook-Pro.local>
2022-11-14 16:39:28 -0500

    Doc: checkpoint, updated weather.js to work with MongoDB

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,28 +1,36 @@
 require('dotenv').config({path: './../../../.env'})
 const axios = require('axios')
+const WEATHER = require('../models/Weather')
 
 const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 class Weather {
     /**
-     * Connect to the API and grab data to send back to the frontend
+     * Saves weather data using the zipcode
+     * If already existing, replace, if not, add
      *
      * @param {number} zipCode The zipcode used to get the weather info from the api
      * @param {string} tempMetric This is either "imperial" (use Fahrenheit) or "metric" (use Celsius)
      * @return {JSON} The data response from the api call
      */
+     saveWeatherDataToDatabase = async(zipCode, data) => {
+        const filter = { zip: zipCode }
+        const replace = {
+            ...filter,
+            ...data,
+            data: Date.now()
+        }
+        await this.findOneReplace(filter, replace)
+    }
 
-    getWeatherData = async(zipCode, tempMetric) => {
-        /**
-         * - get api "By ZIP code" (https://openweathermap.org/current#zip)
-         * - "us" query = "United States"
-         * - "process.env.WEATHER_KEY" is the api key the .env file
-         * - "units" query can be either imperial (Fahrenheit) or metric (Celsius)
-         */
+    // Save data to database
+    getWeatherData = async(zipCode) => {
+        return WEATHER.findOne({ zip: zipCode })
+    }
 
-        let url = `${BASE_URL}?zip=${zipCode},us&appid=${process.env.WEATHER_API_KEY}&units=${tempMetric}`
-
-        return(await axios.get(url)).data
+    // If document already exists with filter, replace, if not, add a new document
+    async findOneReplace(filter, replace) {
+        await WEATHER.findOneAndReplace(filter, replace, { new: true, upsert: true })
     }
 }
 
